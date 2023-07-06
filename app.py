3750b734053e17f43577372f571ba0195733b069
Ruslan Mingaleev <neoch56@mail.ru>
2023-07-06 16:57:49 +0300

    Add recommendations by movie

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import psycopg2
 from dotenv import load_dotenv
 from flask import Flask
 
+from recommendations.by_movie import recommend_by_movie
+
 load_dotenv()
 
 app = Flask(__name__)
@@ -10,6 +12,6 @@ url = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(url)
 
 
-@app.get("/")
-def home():
-    return "hello world"
+@app.get("/recommendations/by-movie/<int:movie_id>")
+def get_recommendations_by_movie(movie_id):
+    return recommend_by_movie(connection, movie_id)
---
+++ recommendations/by_movie.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import scipy.sparse as sp
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def transform_data(data_keywords, data_overviews):
+    count = CountVectorizer()
+    count_matrix = count.fit_transform(data_keywords)
+
+    tfidf = TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(data_overviews)
+
+    combine_sparse = sp.hstack([count_matrix, tfidf_matrix], format='csr')
+    cosine_sim = cosine_similarity(combine_sparse, combine_sparse)
+
+    return cosine_sim
+
+
+def recommend_movies(id, data, transform):
+    indices = pd.Series(data.index, index=data['id'])
+    index = indices[id]
+
+    sim_scores = list(enumerate(transform[index]))
+    sim_scores = sorted(sim_scores, key=lambda x: x[1], reverse=True)
+    sim_scores = sim_scores[1:16]
+
+    movie_indices = [i[0] for i in sim_scores]
+
+    recommendation_data = pd.DataFrame()
+
+    recommendation_data['id'] = data['id'].iloc[movie_indices]
+    recommendation_data['poster_path'] = data['poster_path'].iloc[movie_indices]
+    recommendation_data['release_date'] = data['release_date'].iloc[movie_indices].apply(
+        lambda x: x.strftime('%Y-%m-%dT%H:%M:%S.000Z'))
+    recommendation_data['runtime'] = data['runtime'].iloc[movie_indices]
+    recommendation_data['title'] = data['title'].iloc[movie_indices]
+    recommendation_data['vote_average'] = data['vote_average'].iloc[movie_indices]
+
+    return recommendation_data
+
+
+def recommend_by_movie(connection, movieId):
+    SELECT_MOVIES = """SELECT id, poster_path, release_date, runtime, title, vote_average, overview, keywords.words
+    FROM movies JOIN keywords ON movies.id=keywords.\"movieId\""""
+
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(SELECT_MOVIES)
+            result = cursor.fetchall()
+            movies = pd.DataFrame(
+                result, columns=['id', 'poster_path', 'release_date', 'runtime', 'title', 'vote_average', 'overview', 'keywords'])
+
+    movies['keywords'] = movies['keywords'].fillna('')
+    transform_result = transform_data(movies['keywords'], movies['overview'])
+
+    recommended_movies = recommend_movies(movieId, movies, transform_result)
+
+    recommended_movies.info()
+
+    return recommended_movies.to_json(orient="records")
