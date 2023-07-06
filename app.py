dece14bcbd7816875490f83d43af6647d4f4de34
Ruslan Mingaleev <neoch56@mail.ru>
2023-07-06 15:28:03 +0300

    Connect database

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
 from flask import Flask
 
+load_dotenv()
+
 app = Flask(__name__)
+url = os.getenv("DATABASE_URL")
+connection = psycopg2.connect(url)
 
 
 @app.get("/")
